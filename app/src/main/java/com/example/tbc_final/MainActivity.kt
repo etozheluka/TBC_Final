0c4970754041a01ec163d2ac88f34a269aae244c
Luka <nikuradzeluka0@gmail.com>
2022-10-18 17:56:21 +0400

    xml fixes

--- app/src/main/java/com/example/tbc_final/MainActivity.kt
+++ app/src/main/java/com/example/tbc_final/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tbc_final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -12,11 +13,17 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        supportActionBar?.hide()
+        installSplashScreen()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
