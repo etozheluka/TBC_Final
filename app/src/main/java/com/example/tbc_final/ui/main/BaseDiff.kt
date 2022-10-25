40680ab8ad441d396db2b1ff98c81d1763e5720f
Mariam Namgaladze <mariam.namgaladze.1@btu.edu.ge>
2022-10-25 18:44:49 +0400

    Base Adapter Added

---
+++ app/src/main/java/com/example/tbc_final/ui/main/BaseDiff.kt
@@ -0,0 +1,17 @@
+package com.example.tbc_final.ui.main
+
+abstract class BaseDiff<T> {
+
+    abstract val inner: T
+    abstract val uniqueValue: Any
+    abstract fun compareTo(other: Any?):Boolean
+    override fun equals(other:Any?):Boolean{
+        return compareTo(other)
+    }
+
+    override fun hashCode(): Int {
+        var result = inner?.hashCode() ?:0
+        result = 31 * result+uniqueValue.hashCode()
+        return result
+    }
+}
