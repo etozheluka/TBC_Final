b1552af1ed718f6fd70fd9738699d3f0ad893e7b
Luka <nikuradzeluka0@gmail.com>
2022-10-25 18:50:20 +0400

    navigation fixes

--- app/src/main/java/com/example/tbc_final/ui/main/StartFragment.kt
+++ app/src/main/java/com/example/tbc_final/ui/main/StartFragment.kt
@@ -3,17 +3,8 @@ package com.example.tbc_final.ui.main
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.navigation.fragment.findNavController
-import com.example.tbc_final.R
-import com.example.tbc_final.databinding.FragmentHomeBinding
 import com.example.tbc_final.databinding.FragmentStartBinding
 
 
