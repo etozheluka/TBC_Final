3e6d4a5282255408ac224b188ae93f9f493ad985
Mariam Namgaladze <mariam.namgaladze.1@btu.edu.ge>
2022-10-25 17:51:18 +0400

    Navigaton, Base fragment added

--- app/src/main/java/com/example/tbc_final/ui/main/LogInFragment.kt
+++ app/src/main/java/com/example/tbc_final/ui/main/LogInFragment.kt
@@ -6,17 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tbc_final.R
+import com.example.tbc_final.databinding.FragmentLogInBinding
 
-class LogInFragment : Fragment() {
+class LogInFragment : BaseFragment<FragmentLogInBinding>(FragmentLogInBinding::inflate) {
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
-
-
 }
