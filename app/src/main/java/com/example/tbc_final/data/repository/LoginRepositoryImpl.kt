df1048a1d293c24b30fc5ca6b40a82dbb42289d9
Mariam Namgaladze <mariam.namgaladze.1@btu.edu.ge>
2022-10-30 00:45:17 +0400

    login, registration fixed 2

--- app/src/main/java/com/example/tbc_final/data/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/example/tbc_final/data/repository/LoginRepositoryImpl.kt
@@ -1,6 +1,6 @@
 package com.example.tbc_final.data.repository
 
-import com.example.tbc_final.common.ResponseHandler
+import com.example.tbc_final.common.FirebaseServiceHandler
 import com.example.tbc_final.common.UiState
 import com.example.tbc_final.domain.repository.LoginRepository
 import com.google.firebase.auth.AuthResult
@@ -10,17 +10,15 @@ import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class LoginRepositoryImpl @Inject constructor(private val auth: FirebaseAuth) : LoginRepository,
-    ResponseHandler() {
+class LoginRepositoryImpl @Inject constructor(private val auth: FirebaseAuth) :
+    FirebaseServiceHandler(), LoginRepository {
     override suspend fun login(email: String, password: String): UiState<AuthResult> =
-        withContext(Dispatchers.IO){
+        withContext(Dispatchers.IO) {
             return@withContext try {
-                val result = auth.signInWithEmailAndPassword(email,password).await()
+                val result = auth.signInWithEmailAndPassword(email, password).await()
                 success(result)
-            }catch (e:Exception){
+            } catch (e: Exception) {
                 exceptions(e)
             }
-
-
-    }
+        }
 }
